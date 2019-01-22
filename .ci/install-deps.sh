45fc7fe4f812d533505a58c7693c2cf16abc175f
Brian Devins-Suresh <badevins@gmail.com>
2019-01-22 09:37:45 -0500

    Fix some bash problems

--- .ci/install-deps.sh
+++ .ci/install-deps.sh
@@ -6,8 +6,9 @@ pip install cram
 
 curl -Lo argbash.zip "https://github.com/matejak/argbash/archive/$ARGBASH_VERSION.zip"
 unzip -o argbash.zip
-cd "argbash-$ARGBASH_VERSION"/resources
-make install PREFIX=$HOME/.local
+(
+    cd "argbash-$ARGBASH_VERSION"/resources || exit 1
+    make install PREFIX="$HOME/.local"
+)
 
-cd -
 rm -rf argbash*
