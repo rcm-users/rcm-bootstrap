bbb22e47ce8f68bc60283b72ceb7a00e58a809f6
Brian Devins-Suresh <badevins@gmail.com>
2019-01-22 13:22:35 -0500

    Shellcheck fixes

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,11 +2,11 @@
 
 mkdir -p bin
 (
-    cd bin
+    cd bin || exit 1
     rm -f bootstrap
     rm -f bootstrap-*
     cp ../src/* .
-    j2 -g *.j2 2> /dev/null
-    rm *.j2*
+    j2 -g ./*.j2 2> /dev/null
+    rm ./*.j2*
     for f in * ; do mv -- "$f" "bootstrap-$f" ; done
 )
