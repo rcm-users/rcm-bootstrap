df2dca7cb269b00ff0d096fd1dffdf4bde25d719
Brian Devins-Suresh <badevins@gmail.com>
2022-03-04 10:55:03 -0500

    Fix it

--- .ci/install-deps.sh
+++ .ci/install-deps.sh
@@ -6,4 +6,4 @@ pipenv install --three
 wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
 echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 sudo apt-get update
-sudo apt-get -y install rcm
+sudo apt-get -y install rcm shellcheck
